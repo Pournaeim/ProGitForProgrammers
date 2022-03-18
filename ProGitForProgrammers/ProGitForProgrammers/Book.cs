fd37483e86ac9348196dd39d336e6345243720cd
Sina <pournaeim@gmail.com>
2022-03-18 21:08:01 +0330

    add another line to book.cs

--- ProGitForProgrammers/ProGitForProgrammers/Book.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Book.cs
@@ -10,6 +10,7 @@ namespace ProGitForProgrammers
     {
         public string Title { get; set; }
         public List<string> Authors { get; set; }
+        
         public DateTime PublicationDate { get; set; }
 
     }
